5648d313443c21ea906a9c05a1bf36dfaf25f9f9
SabinaPia <sabinapia2000@gmail.com>
2021-08-14 23:34:48 -0500

    cambios-2

--- views/modules/buscar_evento.php
+++ views/modules/buscar_evento.php
@@ -2,31 +2,6 @@
 
     <section class="seccion contenedor">
 
-        <?php
-        
-            $busqueda = "";
-            $buscar_categoria = "";
-            $buscar_area = "";
-            if(empty($_REQUEST['busqueda']) && empty($_REQUEST['categoria']) && empty($_REQUEST['area'])){
-                header("location: eventos.php");
-            }
-            if(!empty($_REQUEST['busqueda'])){
-                $busqueda = strtolower($_REQUEST['busqueda']);
-                $where = "ev.titulo LIKE '%$busqueda%'";
-                $buscar = 'busqueda='.$busqueda;
-            }
-            if(!empty($_REQUEST['categoria'])){
-                $buscar_categoria = $_REQUEST['categoria'];
-                $where = "ev.id_categoria LIKE $buscar_categoria";
-                $buscar = 'categoria='.$buscar_categoria;
-            }
-            if(!empty($_REQUEST['area'])){
-                $buscar_area = $_REQUEST['area'];
-                $where = "ev.id_area LIKE $buscar_area";
-                $buscar = 'area='.$buscar_area;
-            }
-        ?>
-
         <h2>Eventos Próximos</h2>
 
         <form action="buscar_evento.php" method="get" class="buscar">
@@ -40,25 +15,7 @@
                 <th>Titulo</th>
                 <th>Publicador</th>
                 <th>
-                <?php
-                
-                    $cat = 0;
-                    if(!empty($_REQUEST['categoria'])){
-                        $cat = $_REQUEST['categoria'];
-                    }
-                    
-                    try {
 
-                    require_once('includes/funciones/bd_conexion.php');
-            
-                    $sql = " SELECT * FROM categorias_evento ORDER BY categoria ASC; "; 
-                    $resultado = $conn->query($sql);
-                    } 
-                    catch(Exception $e) {
-                        die('Ha ocurrido un error: ' . $e->getMessage());
-                    }
-
-                ?>
                 <select name="categoria" id="buscar_categoria">
                     <option value="" selected>Categoria</option>
                     <?php 
@@ -82,25 +39,7 @@
 
                 </th>
                 <th>
-                <?php
-                
-                    $ar = 0;
-                    if(!empty($_REQUEST['area'])){
-                        $ar = $_REQUEST['area'];
-                    }
-                    
-                    try {
 
-                    require_once('includes/funciones/bd_conexion.php');
-            
-                    $sql = " SELECT * FROM areas ORDER BY area ASC; "; 
-                    $resultado = $conn->query($sql);
-                    } 
-                    catch(Exception $e) {
-                        die('Ha ocurrido un error: ' . $e->getMessage());
-                    }
-
-                ?>
                 <select name="area" id="buscar_area">
                     <option value="" selected>Area</option>
                     <?php 
@@ -125,52 +64,6 @@
                 </th>
                 <th>Detalle</th>
             </tr>
-
-        <?php
-
-            try {
-
-            require_once('includes/funciones/bd_conexion.php');
-
-            $sql_registro = " SELECT COUNT(*) as total_registro FROM eventos as ev";
-                                                    
-            $sql_registro .= " WHERE $where "; 
-
-            $resultado_registro = $conn->query($sql_registro);
-            $resultado_registros = $resultado_registro->fetch_array(MYSQLI_ASSOC);
-            $total_registro = $resultado_registros['total_registro'];
-
-            $por_pagina = 7; 
-            
-            if(empty($_GET['pagina'])){
-                $pagina = 1;
-            }else{
-                $pagina = $_GET['pagina'];
-            }
-
-            $desde = ($pagina-1) * $por_pagina;
-            $total_pag = ceil($total_registro / $por_pagina);
-
-        
-            $sql = " SELECT id_evento, titulo, publicador, categoria, area "; 
-                        
-            $sql .= " FROM eventos as ev ";
-            $sql .= " INNER JOIN categorias_evento ";
-            $sql .= " ON ev.id_categoria = categorias_evento.id_categoria "; 
-            $sql .= " INNER JOIN publicadores";
-            $sql .= " ON ev.id_publicador = publicadores.id_publicador ";
-            $sql .= " INNER JOIN areas";
-            $sql .= " ON ev.id_area = areas.id_area ";
-            $sql .= " WHERE $where"; 
-            $sql .= " ORDER BY id_evento ASC LIMIT $desde, $por_pagina ";
-
-            $resultado = $conn->query($sql);
-            } 
-            catch(Exception $e) {
-            die('Ha ocurrido un error: ' . $e->getMessage());
-            }
-
-        ?>
 
 
         <?php while($evento = $resultado->fetch_array(MYSQLI_ASSOC)) { ?>
--- views/modules/buscar_revista.php
+++ views/modules/buscar_revista.php
@@ -2,26 +2,6 @@
 
     <section class="seccion contenedor">
 
-        <?php
-            
-            $busqueda = "";
-            $buscar_area = "";
-            if(empty($_REQUEST['busqueda']) && empty($_REQUEST['area'])){
-                header("location: revistas.php");
-            }
-            if(!empty($_REQUEST['busqueda'])){
-                $busqueda = strtolower($_REQUEST['busqueda']);
-                $where = "re.titulo LIKE '%$busqueda%'";
-                $buscar = 'busqueda='.$busqueda;
-            }
-            if(!empty($_REQUEST['area'])){
-                $buscar_area = $_REQUEST['area'];
-                $where = "re.id_area LIKE $buscar_area";
-                $buscar = 'area='.$buscar_area;
-            }
-        ?>
-
-
         <h2>Revistas</h2>
 
         <form action="buscar_revista.php" method="get" class="buscar">
@@ -34,25 +14,6 @@
                 <th>Titulo</th>
                 <th>Publicador</th>
                 <th>
-                <?php
-                
-                    $ar = 0;
-                    if(!empty($_REQUEST['area'])){
-                        $ar = $_REQUEST['area'];
-                    }
-                    
-                    try {
-
-                    require_once('includes/funciones/bd_conexion.php');
-            
-                    $sql = " SELECT * FROM areas ORDER BY area ASC; "; 
-                    $resultado = $conn->query($sql);
-                    } 
-                    catch(Exception $e) {
-                        die('Ha ocurrido un error: ' . $e->getMessage());
-                    }
-
-                ?>
                 <select name="area" id="buscar_area">
                     <option value="" selected>Area</option>
                     <?php 
@@ -76,50 +37,6 @@
                 </th>
                 <th>Detalle</th>
             </tr>
-
-        <?php
-
-            try {
-
-            require_once('includes/funciones/bd_conexion.php');
-
-            $sql_registro = " SELECT COUNT(*) as total_registro FROM revistas as re"; 
-            $sql_registro .= " WHERE $where"; 
-
-            $resultado_registro = $conn->query($sql_registro);
-            $resultado_registros = $resultado_registro->fetch_array(MYSQLI_ASSOC);
-            $total_registro = $resultado_registros['total_registro'];
-
-            $por_pagina = 7;
-            
-            if(empty($_GET['pagina'])){
-                $pagina = 1;
-            }else{
-                $pagina = $_GET['pagina'];
-            }
-
-            $desde = ($pagina-1) * $por_pagina;
-            $total_pag = ceil($total_registro / $por_pagina);
-
-        
-            $sql = " SELECT id_revista, titulo, publicador, area "; 
-                        
-            $sql .= " FROM revistas as re";
-            $sql .= " INNER JOIN publicadores";
-            $sql .= " ON re.id_publicador = publicadores.id_publicador ";
-            $sql .= " INNER JOIN areas";
-            $sql .= " ON re.id_area = areas.id_area ";
-            $sql .= " WHERE $where"; 
-            $sql .= " ORDER BY id_revista ASC LIMIT $desde, $por_pagina ";
-
-            $resultado = $conn->query($sql);
-            } 
-            catch(Exception $e) {
-            die('Ha ocurrido un error: ' . $e->getMessage());
-            }
-
-        ?>
-
 
         <?php while($revista = $resultado->fetch_array(MYSQLI_ASSOC)) { ?>
             <tr>
